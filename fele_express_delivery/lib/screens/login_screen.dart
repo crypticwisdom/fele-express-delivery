c948452a7df45dd3587b775ed86f271da5c13cbc
Cryptic Wisdom <wisdom@tm30.net>
2023-08-29 07:41:29 +0100

    started login ui

--- fele_express_delivery/lib/screens/login_screen.dart
+++ fele_express_delivery/lib/screens/login_screen.dart
@@ -12,6 +12,28 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('wow');
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        // padding: EdgeInsets.all(20),
+        child: Column(children: [
+          Row(
+            children: [
+              Icon(Icons.forward),
+            ],
+          ),
+          Text("Great to have you back!"),
+          TextField(),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Login"),
+          )
+        ]),
+      ),
+    );
   }
 }
