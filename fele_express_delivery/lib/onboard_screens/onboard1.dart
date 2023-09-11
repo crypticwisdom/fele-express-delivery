d245bd6f608fa1269cd8f2f653b73c4b5454422f
Cryptic Wisdom <wisdom@tm30.net>
2023-09-11 22:25:31 +0100

    commit

---
+++ fele_express_delivery/lib/onboard_screens/onboard1.dart
@@ -0,0 +1,18 @@
+import 'package:fele_express_delivery/layouts/responsive_layout.dart';
+import 'package:flutter/material.dart';
+
+class OnboardScreen1 extends StatelessWidget {
+  const OnboardScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.green,
+      body: ResponsiveLayout(
+          mobileLayout: ,
+          tabletLayout: ,
+          desktopLayout: ,
+        ),
+    );
+  }
+}
